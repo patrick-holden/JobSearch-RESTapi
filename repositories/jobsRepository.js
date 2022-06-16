217dce1a1fe2f56be4f04025af4825dab86cdb6d
danieljporter21 <danieljporter21@googlemail.com>
2022-06-16 16:40:52 +0100

    updated getJobs method

--- repositories/jobsRepository.js
+++ repositories/jobsRepository.js
@@ -5,7 +5,7 @@ dbService.connectToDb().then((db) => jobsCollection = db.collection('jobs'));
 
 const getJob = async (id) => {
     console.log('Repository: getJob ' + id);
-    return await jobsCollection.query('SELECT * FROM products WHERE ID = ' + req.params.id + ';');
+    return await jobsCollection.query('SELECT * FROM jobs WHERE ID = ' + req.params.id + ';');
 }
 
 module.exports.getJob = getJob;
