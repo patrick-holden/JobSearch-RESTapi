a53f30080995ab36e8d3949a65ca2f3259ec74d0
gracesessions <98703832+gracesessions@users.noreply.github.com>
2022-06-17 09:45:45 +0100

    select statement

--- repositories/jobsRepository.js
+++ repositories/jobsRepository.js
@@ -1,11 +1,26 @@
 const dbService = require('../services/dbService');
 
-// let jobsCollection = null;
-// dbService.connectToDb().then((db) => db);
-
 const getJobs = async() => {
     console.log('Repository: getJobs');
-    return await dbService.connectToDb().then((db) => db.query('SELECT * FROM jobs;'));
+    return await dbService.connectToDb().then((db) => db.query(
+    'SELECT `jobs`.`id`, ' +
+        '`jobs`.`job_title`, ' +
+        '`jobs`.`company`, ' +
+        '`jobs`.`logo`,' +
+        '`jobs`.`salary`,' +
+        '`jobs`.`type`, ' +
+        '`skills`.`skill` ' +
+        'FROM `jobs` ' +
+        'RIGHT JOIN ' +
+        '`jobs_skills` ' +
+        'ON `jobs`.`id` = `jobs_skills`.`job_id` ' +
+        'LEFT JOIN `skills` ' +
+        'ON `jobs_skills`.`skill_id` = `skills`.`id`' +
+        'WHERE `jobs`.`id` = ' + 4 + ';'
+    ));
+
+    
+
 }
 
 module.exports.getJobs = getJobs;
