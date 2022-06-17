004214c1e89c381fc444f6b1995a732d6ac0bd6b
patrick-holden <holden.patrick@gmail.com>
2022-06-17 11:11:55 +0100

    add skill values to array of skills

--- repositories/jobsRepository.js
+++ repositories/jobsRepository.js
@@ -2,7 +2,7 @@ const dbService = require('../services/dbService');
 
 const getJobs = async() => {
     console.log('Repository: getJobs');
-    const allJobs =  await dbService.connectToDb().then((db) => db.query(
+    const allRecords =  await dbService.connectToDb().then((db) => db.query(
       'SELECT `jobs`.`id`, ' +
       '`jobs`.`job_title`, ' +
       '`jobs`.`company`, ' +
@@ -18,11 +18,23 @@ const getJobs = async() => {
       'ON `jobs_skills`.`skill_id` = `skills`.`id`' +
       'WHERE `jobs`.`id` = ' + 4 + ';'
     ));
+    let allJobs = [];
+    let previousId = -1;
+    allRecords.forEach((record) => {
+        let lastJob = allJobs[allJobs.length-1];
+        // console.log(lastJob);
+        if (record['id'] !== previousId) {
+            previousId = record['id'];
+            record['skill'] = [record['skill']];
+            allJobs.push(record);
+        } else {
+            lastJob['skill'].push(record['skill']);
+            console.log(lastJob);
+            console.log(record['skill']);
 
-    allJobs.forEach((job) => {
-    if (job['id'] == 4) {
-        console.log('hello');
     }})
+    console.log(allJobs);
+    // console.log(jobSkills);
     return allJobs;
 }
 module.exports.getJobs = getJobs;
