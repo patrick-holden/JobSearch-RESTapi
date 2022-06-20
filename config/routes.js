005180790fd440c10b1a55a068abe105c90c3f9a
gracesessions <98703832+gracesessions@users.noreply.github.com>
2022-06-20 09:42:20 +0100

    invalid routes

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,10 @@ const jobsController = require('../controllers/jobsController');
 const routes = (app) => {
     app.get('/jobs', jobsController.getJobs);
     app.get('/jobs/:jobId', jobsController.getJob);
+    app.get('*', (req, res) => {
+        res.sendStatus(404);
+        res.json(JSON.stringify({message: 'Invalid request'}));
+    })
 }
 
 module.exports = routes;
