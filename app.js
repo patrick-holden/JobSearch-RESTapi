bb6869d2a5635a0cbf2828ce46335be82e31d1ef
patrick-holden <holden.patrick@gmail.com>
2022-06-21 15:51:58 +0100

    partner sha1 authentication added

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const routes = require('./config/routes')
+const routes = require('./config/routes');
+const tokenCheck = require('./services/authTokenService');
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(tokenCheck);
 
 routes(app);
 
