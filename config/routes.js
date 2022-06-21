bc45536f4ae1e4aad94c31f32a734f6272391415
patrick-holden <holden.patrick@gmail.com>
2022-06-21 13:48:08 +0100

    local changes

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,7 @@ const jobsController = require('../controllers/jobsController');
 const routes = (app) => {
     app.get('/jobs', jobsController.getJobs);
     app.get('/jobs/:jobId', jobsController.getJob);
-    app.get('/jobs/search/:term', jobsController.getSearchJobs);
+    // app.get('/jobs/search/:term', jobsController.getSearchJobs);
     app.get('/jobs/admin/:term', (req, res) => {res.send('admin')});
     app.get('*', (req, res) => {
         res.sendStatus(404);
