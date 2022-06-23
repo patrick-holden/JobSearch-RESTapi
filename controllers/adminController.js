19838db2da12139b3201f246fc7e2f762f10b289
gracesessions <98703832+gracesessions@users.noreply.github.com>
2022-06-23 16:31:42 +0100

    tested both santise functions

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -17,8 +17,9 @@ const getJobs = (req, res) => {
   }
 
   adminService.getAllJobsData(query, req, res).then((allJobs) => {
-
-    if(Object.keys(allJobs).length === 0) {
+    if(allJobs === -1) {
+      res.status(404).json(httpResponseService(res.statusCode,'Invalid Search',true))
+    } else if(Object.keys(allJobs).length === 0) {
       res.json(httpResponseService(res.statusCode, 'No jobs found',true, allJobs))
     } else {
       res.json(httpResponseService(res.statusCode,'Success',true,  allJobs));
@@ -30,7 +31,9 @@ const markJobFilled = (req, res) => {
   let jobId = parseInt(req.params.jobId);
   adminService.markJobFilled(jobId).then((id) => {
     let lastId = parseInt(id)
-    if (lastId !== jobId) {
+    if(lastId === -1) {
+      res.status(404).json(httpResponseService(res.statusCode,'TypeError in Id',true))
+    } else if (lastId !== jobId) {
       res.json(httpResponseService(res.statusCode,'unsuccessful',true, ))
     } else {
       res.json(httpResponseService(res.statusCode,'success',true,))
