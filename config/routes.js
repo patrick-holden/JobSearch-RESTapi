8854909c51e5e1f4379fc876be1cbfc2d3e57c9b
patrick-holden <holden.patrick@gmail.com>
2022-06-23 10:00:39 +0100

    auth token refactor

--- config/routes.js
+++ config/routes.js
@@ -3,19 +3,19 @@ const adminController = require('../controllers/adminController');
 const httpResponseService = require('../services/httpResponseService');
 const authTokenService = require("../services/authTokenService");
 const {checkAdminToken} = require("../services/authTokenService");
-
+const invalidRequestController = require('../controllers/invalidRequestController');
 
 const routes = (app) => {
     app.get('/jobs', authTokenService.checkPartnerToken, jobsController.getJobs);
-    app.get('/jobs/query/', authTokenService.checkPartnerToken, jobsController.getSearchAndFilterJobs);
     app.get('/jobs/:jobId', authTokenService.checkPartnerToken, jobsController.getJob);
+    app.get('/jobs/query/', authTokenService.checkPartnerToken, jobsController.getSearchAndFilterJobs);
     app.get('/admin/jobs', authTokenService.checkAdminToken, adminController.getAllJobsData);
     app.post('/admin/jobs/filljob/:jobId', checkAdminToken, adminController.postFilledJob)
 
-
-    app.get('*', (req, res) => {
-        res.status(404).json(httpResponseService(res.statusCode, 'Invalid Request'));
-    })
+    app.get('*', invalidRequestController.httpInvalid);
+    app.post('*', invalidRequestController.httpInvalid);
+    app.put('*', invalidRequestController.httpInvalid);
+    app.delete('*', invalidRequestController.httpInvalid);
 }
 
 module.exports = routes;
