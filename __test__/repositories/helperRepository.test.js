6185af61cbf2f30c495c1781068ba6222c98a9a6
patrick-holden <holden.patrick@gmail.com>
2022-06-23 16:10:56 +0100

    skill empty unit test

--- __test__/repositories/helperRepository.test.js
+++ __test__/repositories/helperRepository.test.js
@@ -8,19 +8,31 @@ describe('sqlEdit function', () => {
         }
         expect(sqlEdit('jobs', query)).toStrictEqual({
             "searchParams": [],
-            "sql": "SELECT `jobs`.`id`, `jobs`.`job_title`," +
-                " `jobs`.`company`, `jobs`.`logo`,`jobs`.`salary`," +
-                "`jobs`.`type`, `skills`.`skill` FROM `jobs` " +
-                "LEFT JOIN `jobs_skills` ON `jobs`.`id` = `jobs_skills`.`job_id` " +
-                "LEFT JOIN `skills` ON `jobs_skills`.`skill_id` = `skills`.`id` " +
-                "WHERE `jobs_skills`.`skill_id` = '5';"
+            "sql": "SELECT `jobs`.`id`, `jobs`.`job_title`, `jobs`.`company`, `jobs`.`logo`,`jobs`.`salary`," +
+                "`jobs`.`type`, `skills`.`skill` FROM `jobs` LEFT JOIN `jobs_skills` ON `jobs`.`id` = `jobs_skills`.`job_id` " +
+                "LEFT JOIN `skills` ON `jobs_skills`.`skill_id` = `skills`.`id` WHERE `jobs_skills`.`skill_id` = '5';"
         })
     })
-    it('given jobs table and skill filter 5', () => {
+
+    it('given jobs table and skill filter with no value', () => {
+        const query = {
+            skill: ''
+        }
+        expect(sqlEdit('jobs', query)).toStrictEqual({
+            "searchParams": [],
+            "sql": "SELECT `jobs`.`id`, `jobs`.`job_title`, `jobs`.`company`, `jobs`.`logo`,`jobs`.`salary`," +
+              "`jobs`.`type`, `skills`.`skill` FROM `jobs` LEFT JOIN `jobs_skills` ON `jobs`.`id` = `jobs_skills`.`job_id` " +
+              "LEFT JOIN `skills` ON `jobs_skills`.`skill_id` = `skills`.`id` WHERE `jobs_skills`.`skill_id` = '';"
+        })
+    })
+
+    it('given jobs table and search input junior', () => {
         const query = {
             search: 'junior'
         }
         expect(sqlEdit('jobs', query)).toStrictEqual({"searchParams": ["%junior%", "%junior%"],"sql": "SELECT `jobs`.`id`, `jobs`.`job_title`, `jobs`.`company`, `jobs`.`logo`,`jobs`.`salary`,`jobs`.`type`, `skills`.`skill` FROM `jobs` LEFT JOIN `jobs_skills` ON `jobs`.`id` = `jobs_skills`.`job_id` LEFT JOIN `skills` ON `jobs_skills`.`skill_id` = `skills`.`id` WHERE ( `jobs`.`job_description` LIKE ? OR `jobs`.`job_title` LIKE ?);"
         })
     })
+
+
 })
